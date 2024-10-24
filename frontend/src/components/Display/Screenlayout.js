559c534bbbf8590222b5999437f785b981bfd94b
Grant <grant@Grants-MacBook-Pro.local>
2024-10-24 17:44:34 +0200

    Update

--- frontend/src/components/Display/Screenlayout.js
+++ frontend/src/components/Display/Screenlayout.js
@@ -3,9 +3,11 @@ import { TopBar } from "components/Display/TopBar";
 import { ScreenLayoutInner } from "./ScreenLayoutInner";
 import { SideBar } from "./SideBar";
 import { Slide } from "@mui/material";
+import { Size } from "media-query";
 
 export const ScreenLayout = () => {
   const [topBarOn, setTopBarOn] = useState(true);
+  var size = Size();
 
   const toggleTopBar = () => {
     setTopBarOn(!topBarOn);
@@ -22,33 +24,52 @@ export const ScreenLayout = () => {
     >
       <TopBar setTopBarOn={toggleTopBar} />
       {topBarOn ? (
-        <div style={{ display: "flex", flexGrow: 1 }}>
-          <Slide in={topBarOn} timeout={1200} direction="right">
-            <div
-              style={{
-                position: "fixed", // Fix the sidebar
-                top: "1rem", // Position below the TopBar
-                left: 0,
-                width: "200px",
-                backgroundColor: "#406885",
-                height: "calc(100vh - 1rem)", // Full height minus TopBar height
-                overflowY: "auto", // Enable scrolling if needed
-              }}
-            >
-              <SideBar />
+        <div>
+          {size == "XS" || size == "SM" || size == "MD" ? (
+            <div className="block">
+              <Slide in={topBarOn} timeout={1200} direction="down">
+                <div
+                  style={{
+                    backgroundColor: "#406885",
+                    paddingBottom: "20pt",
+                  }}
+                >
+                  <SideBar setTopBarOn={toggleTopBar} />
+                </div>
+              </Slide>
+              <div>
+                <ScreenLayoutInner />
+              </div>
             </div>
-          </Slide>
-
-          <div
-            style={{
-              marginLeft: "200px", // Leave space for the fixed sidebar
-              flex: "1",
-              height: "100%", // Full height
-              overflowY: "auto", // Enable scrolling if needed
-            }}
-          >
-            <ScreenLayoutInner />
-          </div>
+          ) : (
+            <div style={{ display: "flex", flexGrow: 1 }}>
+              <Slide in={topBarOn} timeout={1200} direction="right">
+                <div
+                  style={{
+                    position: "fixed", // Fix the sidebar
+                    top: "1rem", // Position below the TopBar
+                    left: 0,
+                    width: "200px",
+                    backgroundColor: "#406885",
+                    height: "calc(100vh - 1rem)", // Full height minus TopBar height
+                    overflowY: "auto", // Enable scrolling if needed
+                  }}
+                >
+                  <SideBar setTopBarOn={toggleTopBar} />
+                </div>
+              </Slide>
+              <div
+                style={{
+                  marginLeft: "200px", // Leave space for the fixed sidebar
+                  flex: "1",
+                  height: "100%", // Full height
+                  overflowY: "auto", // Enable scrolling if needed
+                }}
+              >
+                <ScreenLayoutInner />
+              </div>
+            </div>
+          )}
         </div>
       ) : (
         <div>
--- frontend/src/components/Display/SideBar.js
+++ frontend/src/components/Display/SideBar.js
@@ -1,23 +1,26 @@
 import React from "react";
 import { Button } from "@mui/material";
 import { TopBarButtonStyle } from "./feutures";
+import { Routes } from "components/Routes";
+import { Size } from "media-query";
 
-export const SideBar = () => {
+export const SideBar = ({ setTopBarOn }) => {
+  const size = Size();
+  const isResponsiveSize = ["XS", "SM", "MD"].includes(size);
+  
   return (
     <div className="flex align-center justify-center">
-      <div className="grid grid-flow-row gap-2 mt-24">
-        <Button href={"/home"} sx={TopBarButtonStyle}>
-          Home
-        </Button>
-        <Button href={"/solutions"} sx={TopBarButtonStyle}>
-          Solutions
-        </Button>
-        <Button href={"/about"} sx={TopBarButtonStyle}>
-          About
-        </Button>
-        <Button href={"/contact"} sx={TopBarButtonStyle}>
-          Contact
-        </Button>
+      <div className={`grid grid-flow-${isResponsiveSize ? 'col' : 'row'} gap-2 mt-24`}>
+        {Routes.map((page) => (
+          <Button
+            key={page.path} // Add a key prop for better list rendering
+            onClick={setTopBarOn}
+            href={page.path}
+            sx={TopBarButtonStyle}
+          >
+            {page.name}
+          </Button>
+        ))}
       </div>
     </div>
   );
